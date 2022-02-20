6b7ad31ccc80838796a731954ed369f83fe2a56e
Matías Ramírez <matias.ramirezg@sansano.usm.cl>
2022-02-19 23:33:13 -0300

    add auth context store

---
+++ src/store/auth-context.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useCookies } from "react-cookie";
+
+const AuthContext = React.createContext();
+
+export const AuthContextProvider = (props) => {
+  const [cookies, setCookie, removeCookie] = useCookies([
+    "token",
+    "isLoggedIn",
+  ]);
+
+  const login = (data) => {
+    const token = data.token.split(" ")[1];
+    setCookie("token", token, {
+      path: "/",
+      expires: new Date(new Date().getTime() + 1000 * 86400),
+    });
+    setCookie("isLoggedIn", true, {
+      path: "/",
+      expires: new Date(new Date().getTime() + 1000 * 86400),
+    });
+  };
+  const logout = () => {
+    removeCookie("token", { path: "/" });
+    removeCookie("isLoggedIn", { path: "/" });
+  };
+
+  const context = {
+    isLoggedIn: cookies?.isLoggedIn,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={context}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
